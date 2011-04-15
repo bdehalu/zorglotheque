4ee6865af046c52784cd64643b34c618f7fa4000
Benjamin Dehalu <benjamin@ZorgloMac.local>
2011-04-15 20:49:35 +0200

    Initial Commit

---
+++ zorglotheque/ZTDocument.h
@@ -0,0 +1,16 @@
+//
+//  ZTDocument.h
+//  zorglotheque
+//
+//  Created by Benjamin Dehalu on 15/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ZTDocument : NSPersistentDocument {
+@private
+
+}
+
+@end
---
+++ zorglotheque/ZTDocument.m
@@ -0,0 +1,36 @@
+//
+//  ZTDocument.m
+//  zorglotheque
+//
+//  Created by Benjamin Dehalu on 15/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ZTDocument.h"
+
+@implementation ZTDocument
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Add your subclass-specific initialization here.
+        // If an error occurs here, send a [self release] message and return nil.
+    }
+    return self;
+}
+
+- (NSString *)windowNibName
+{
+    // Override returning the nib file name of the document
+    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
+    return @"ZTDocument";
+}
+
+- (void)windowControllerDidLoadNib:(NSWindowController *)aController
+{
+    [super windowControllerDidLoadNib:aController];
+    // Add any code here that needs to be executed once the windowController has loaded the document's window.
+}
+
+@end
